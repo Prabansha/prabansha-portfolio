672dcb7ffcb731267e68b0ed5a430dbd0b50e955
Prabansha Disanayaka <114307046+Prabansha@users.noreply.github.com>
2025-06-16 13:43:33 +0530

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This will be updated if you're using a repository name as subdirectory
+  base: '/prabansha-portfolio/', // This will be updated if you're using a repository name as subdirectory
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
